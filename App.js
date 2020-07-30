6823d8d163207965b3e48aad8384447fe4af2248
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-07-30 16:49:48 +0200

    add login screen
    Created a login screen with Formik

--- App.js
+++ App.js
@@ -1,39 +1,7 @@
 import React, { useState } from "react";
 
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import AppButton from "./app/components/AppButton";
-import Card from "./app/components/Card";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import { TextInput, Text, Switch } from "react-native";
-import AppTextInput from "./app/components/AppTextInput";
-import AppPicker from "./app/components/AppPicker";
-
-const categories = [
-  { label: "Furniture", value: 1 },
-  { label: "Clothing", value: 1 },
-  { label: "Cameras", value: 1 },
-];
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
-
-  return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        items={categories}
-        icon="apps"
-        placeholder="Category"
-      />
-      <AppTextInput icon="email" placeholder="Email" />
-    </Screen>
-  );
+  return <LoginScreen></LoginScreen>;
 }
---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { StyleSheet, Image } from "react-native";
+import { Formik } from "formik";
+
+import Screen from "../components/Screen";
+import AppTextInput from "../components/AppTextInput";
+import AppButton from "../components/AppButton";
+
+function LoginScreen() {
+  return (
+    <Screen style={styles.container}>
+      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleSubmit }) => (
+          <>
+            <AppTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="email"
+              onChangeText={handleChange("email")}
+              keyboardType="email-address"
+              placeholder="Email"
+              textContentType="emailAddress"
+            />
+            <AppTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="lock"
+              onChangeText={handleChange("password")}
+              placeholder="Password"
+              secureTextEntry
+              textContentType="password"
+            />
+            <AppButton title="Login" onPress={handleSubmit} />
+          </>
+        )}
+      </Formik>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
+
+export default LoginScreen;
