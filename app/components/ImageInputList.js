044ad06a3bf79587596f39d171f836414fe84b37
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-08-05 17:53:30 +0200

    Add a scroll to the ImageInputList
    Add a scroll to the ImageInputList and make it scroll to the end of the list using useRef and onContentSizeChange

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,23 +1,33 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useRef } from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
 import ImageInput from "./ImageInput";
 
 function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
+  const scrollView = useRef();
+
   return (
-    <View style={styles.container}>
-      {
-        //If you have an image inside an ImageInput and the onChangeImage event is raised that means the user has deleted that image.
-        //We don't want to maintain local state in this component, therefore we use onRemoveImage.
-        imageUris.map((uri) => (
-          <View key={uri} style={styles.image}>
-            <ImageInput
-              imageUri={uri}
-              onChangeImage={() => onRemoveImage(uri)}
-            />
-          </View>
-        ))
-      }
-      <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+    <View>
+      <ScrollView
+        ref={scrollView}
+        horizontal
+        onContentSizeChange={() => scrollView.current.scrollToEnd()}
+      >
+        <View style={styles.container}>
+          {
+            //If you have an image inside an ImageInput and the onChangeImage event is raised that means the user has deleted that image.
+            //We don't want to maintain local state in this component, therefore we use onRemoveImage.
+            imageUris.map((uri) => (
+              <View key={uri} style={styles.image}>
+                <ImageInput
+                  imageUri={uri}
+                  onChangeImage={() => onRemoveImage(uri)}
+                />
+              </View>
+            ))
+          }
+          <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+        </View>
+      </ScrollView>
     </View>
   );
 }
