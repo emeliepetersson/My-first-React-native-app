89da9f21c3d38ed128a83631d043fabb6ba07820
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-08-05 18:20:28 +0200

    add the form image picker to listing edit screen

--- App.js
+++ App.js
@@ -1,28 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
-import Screen from "./app/components/Screen";
-import ImageInputList from "./app/components/ImageInputList";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  const [imageUris, setImageUris] = useState([]);
-
-  const handleAdd = (uri) => {
-    //With the spread operator we take a copy of the imageUri, and then add the uri at the end
-    setImageUris([...imageUris, uri]);
-  };
-
-  const handleRemove = (uri) => {
-    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
-  };
-
-  return (
-    <Screen>
-      <ImageInputList
-        imageUris={imageUris}
-        //onAddImage takes an uri which is sent to the function handleAdd
-        onAddImage={handleAdd}
-        onRemoveImage={handleRemove}
-      />
-    </Screen>
-  );
+  return <ListingEditScreen />;
 }
