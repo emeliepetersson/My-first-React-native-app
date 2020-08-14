93d9e690d194fb37c133dda783c1764c04251a4c
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-08-14 13:48:09 +0200

    add a cache layer
    The cache layer is going to automatically serialise/deserialise(stringyfy, parse) objects so we don’t have to call JSON.stringyfy/JSON.parse all the time. Our cache is also going to apply a timestamp to each item, and then figure out if the item is expired or not. If expired, it can automatically clean up async storage and free up som space.

--- App.js
+++ App.js
@@ -1,87 +1,14 @@
 import React from "react";
-import { Text, Button } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import Screen from "./app/components/Screen";
-import AuthNavigator from "./app/navigation/AuthNavigator";
 import NavigationTheme from "./app/navigation/NavigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
-
-// const Link = () => {
-//   const navigation = useNavigation();
-
-//   return (
-//     <Button title="click" onPress={() => navigation.navigate("TweetDetails")} />
-//   );
-// };
-
-// const Tweets = ({ navigation }) => {
-//   return (
-//     <Screen>
-//       <Text>Tweets</Text>
-//       <Button
-//         title="Tweet Details"
-//         onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
-//       />
-//     </Screen>
-//   );
-// };
-
-// const TweetDetails = ({ route }) => {
-//   return (
-//     <Screen>
-//       <Text>Tweet Details {route.params.id}</Text>
-//     </Screen>
-//   );
-// };
-
-// const Stack = createStackNavigator();
-// const FeedNavigator = () => {
-//   return (
-//     <Stack.Navigator
-//       screenOptions={{
-//         headerStyle: { backgroundColor: "dodgerblue" },
-//         headerTintColor: "white",
-//       }}
-//     >
-//       <Stack.Screen name="Tweets" component={Tweets} />
-//       <Stack.Screen
-//         name="TweetDetails"
-//         component={TweetDetails}
-//         options={
-//           (({ route }) => ({ title: route.params.id }),
-//           {
-//             headerStyle: { backgroundColor: "tomato" },
-//             headerTintColor: "white",
-//           })
-//         }
-//       />
-//     </Stack.Navigator>
-//   );
-// };
-
-// const Account = () => {
-//   return (
-//     <Screen>
-//       <Text>Account</Text>
-//     </Screen>
-//   );
-// };
-
-// const Tab = createBottomTabNavigator();
-// const TabNavigator = () => {
-//   return (
-//     <Tab.Navigator>
-//       <Tab.Screen name="Feed" component={FeedNavigator} />
-//       <Tab.Screen name="Account" component={Account} />
-//     </Tab.Navigator>
-//   );
-// };
+import { View } from "react-native";
 
 export default function App() {
+  NetInfo.addEventListener((netInfo) => console.log(netInfo));
+
   return (
     <NavigationContainer theme={NavigationTheme}>
       <AppNavigator />
